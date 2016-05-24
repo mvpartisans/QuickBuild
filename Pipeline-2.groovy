e470d1d74fc29c1cb196b79504ba9d770d267c07
imranansari <imran.iansari@gmail.com>
2016-05-23 21:28:25 -0700

    init

---
+++ Pipeline-2.groovy
@@ -0,0 +1,18 @@
+import groovy.json.JsonSlurper
+
+
+def buildLib = fileLoader.fromGit('BuildLibNonP.groovy',
+        'https://github.com/mvpartisans/QuickBuildLibs.git', 'master', null, '')
+
+
+node {
+
+    buildLib.coreBuild {[
+            "lfs-a": "May-Release",
+            //"lfs-b": "May-Release",
+            "lfs-c": "newBranch"
+            //"lfs-d": "newBranch"
+    ]}
+
+}
+
