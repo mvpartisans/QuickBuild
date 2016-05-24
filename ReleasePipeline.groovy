679c5b8640b73b45530fce47f5c8e6af74bcf0c4
imranansari <imran.iansari@gmail.com>
2016-05-24 14:30:15 -0700

    init

---
+++ ReleasePipeline.groovy
@@ -0,0 +1,17 @@
+import groovy.json.JsonSlurper
+
+
+def buildLib = fileLoader.fromGit('BuildLibRelease.groovy',
+        'https://github.com/mvpartisans/QuickBuildLibs.git', 'master', null, '')
+
+
+node {
+
+    buildLib.coreBuild {[
+            "lfs-a": "master",
+            "lfs-b": "master"
+    ]}
+
+    buildLib.deploy(['host1']);
+
+}
